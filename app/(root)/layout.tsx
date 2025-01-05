928517517295aade8e4f2ccd753509a6405c987c
Chandramali Thilakaratne <sahanpradeeptha@gmail.com>
2025-01-05 23:44:45 +0530

    second commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+import { Inter, IBM_Plex_Serif } from "next/font/google"
+
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+
+    </main>
+      
+  );
+}
