88be23d23160b51fe9d8b124f0d6ee60b51f1aae
IT21018596 <it21018596@my.sliit.lk>
2025-01-07 16:44:14 +0530

    home page initiation

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../globals.css";
 import { Inter, IBM_Plex_Serif } from "next/font/google"
 
 
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = { firstName: 'Sahan', lastName: 'Thilakaratne' }
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting"
+          title = "welcome"
+          user= {loggedIn?.firstName || 'Guest'}
+          subtext='Create and manage your account'/>
+        </header>
+      </div>
+      
+    </section>
   )
 }
 
