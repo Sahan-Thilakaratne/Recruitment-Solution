f0e8ea34b4c3b5277241d1b2bda432024913cb62
Chandramali Thilakaratne <sahanpradeeptha@gmail.com>
2025-01-08 23:28:23 +0530

    homebox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,13 +4,14 @@ import React from 'react'
 const Home = () => {
 
   const loggedIn = { firstName: 'Sahan', lastName: 'Thilakaratne' }
+
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
           <HeaderBox 
           type="greeting"
-          title = "welcome"
+          title = "Welcome"
           user= {loggedIn?.firstName || 'Guest'}
           subtext='Create and manage your account'/>
         </header>
